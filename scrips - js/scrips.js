5dbea37cfd178d1c68f850091ed64478945d7eed
nicole <agusnicoleruiz@gmail.com>
2024-07-16 23:06:11 -0300

    ..

--- scrips - js/scrips.js
+++ scrips - js/scrips.js
@@ -74,3 +74,26 @@ document.addEventListener('DOMContentLoaded', () => {
         google.maps.event.trigger(map, 'resize');
     });
 });
+
+
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    loop: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  
+    // And if we need scrollbar
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+  });
