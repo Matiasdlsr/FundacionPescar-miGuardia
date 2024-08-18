ff15e66d2f086621369b95181d71314d5d2e909a
nicole <agusnicoleruiz@gmail.com>
2024-08-18 10:54:14 -0300

    Arreglo de los slides

---
+++ scrips - js/scripts.js
@@ -0,0 +1,11 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const swiperContainer = document.querySelector('.swiper-container');
+    const nextButton = document.querySelector('.custom-button-next');
+    const prevButton = document.querySelector('.custom-button-prev');
+
+    if (swiperContainer && nextButton && prevButton) {
+        // Tu código para manejar los botones y la funcionalidad de Swiper
+    } else {
+        console.error('Los elementos no están presentes en el DOM.');
+    }
+});
